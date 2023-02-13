2c7eb81c6c59d06664bcab231a9ad03b8cdd4bfc
Baráth Dávid <davidbarath08@gmail.com>
2023-02-13 10:37:27 +0100

    táblázatba megjeleintve

--- desktop/src/views/MainConsole.java
+++ desktop/src/views/MainConsole.java
@@ -12,7 +12,12 @@ public class MainConsole {
     public void printEmployees(ArrayList<Employee> emps)
     {
         for(Employee emp : emps){
-            System.out.println(emp.getName());
+            System.out.printf(
+            "|%15s |%15s | %8.0f |\n",
+            emp.getName(),
+            emp.getCity(),
+            emp.getSalary()
+            );
         }
     }
     
